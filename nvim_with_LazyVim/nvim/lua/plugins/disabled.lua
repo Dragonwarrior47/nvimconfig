012082284d5450b7b01a73de969a8e3fc02a0a23
Dragonwarrior47 <kanhaiya.yadav47@gmail.com>
2024-01-06 23:18:45 +0530

    Add LazyVime

---
+++ nvim_with_LazyVim/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = true,
+  },
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = true,
+  },
+}
